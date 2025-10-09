6155f766c63401becafe58e7d315d85a04f63426
Gyabu <lr.jgportillo@mmdc.mcl.edu.ph>
2025-10-10 05:51:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mobiledev_unifime/get_started_frame.java
@@ -0,0 +1,37 @@
+package com.example.mobiledev_unifime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class get_started_frame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.get_started_frame);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Get reference to the button
+        Button getStartedButton = findViewById(R.id.button);
+
+        // Set click listener
+        getStartedButton.setOnClickListener(v -> {
+            // Intent to move to the next frame
+            Intent intent = new Intent(get_started_frame.this, login_frame.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mobiledev_unifime/login_frame.java
@@ -0,0 +1,34 @@
+package com.example.mobiledev_unifime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login_frame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_frame);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button createAccountButton = findViewById(R.id.createAccountButton);
+
+        createAccountButton.setOnClickListener(v -> {
+            Intent intent = new Intent(login_frame.this, create_account_frame.class);
+            startActivity(intent);
+        });
+    }
+}
