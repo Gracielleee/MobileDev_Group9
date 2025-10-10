595a0f48fd62f373d614b5af17ba9083196c22c0
Gracielleee <158242680+Gracielleee@users.noreply.github.com>
2025-10-10 14:15:11 +0800

    added activity_main frames
    added menu also

---
+++ app/src/main/java/com/example/mobiledev_unifime/activity_main_fragment_contacts.java
@@ -0,0 +1,16 @@
+package com.example.mobiledev_unifime;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class activity_main_fragment_contacts extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main_fragment_contacts, container, false);
+    }
+}
