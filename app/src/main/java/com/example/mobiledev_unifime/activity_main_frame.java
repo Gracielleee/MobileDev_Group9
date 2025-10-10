140ae80cbb9dae7551cfbb4c9cef3bc041d1146e
Gracielleee <158242680+Gracielleee@users.noreply.github.com>
2025-10-10 20:39:39 +0800

    Added FAB

--- app/src/main/java/com/example/mobiledev_unifime/activity_main_frame.java
+++ app/src/main/java/com/example/mobiledev_unifime/activity_main_frame.java
@@ -19,19 +19,19 @@ public class activity_main_frame extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 // Handle item selection
-                switch (item.getItemId()) {
-                    case R.id.nav_contacts:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new activity_main_fragment_contacts()).commit();
-                        return true;
-                    case R.id.nav_notes:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new NotesFragment()).commit();
-                        return true;
-                    case R.id.nav_schedule:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new ScheduleFragment()).commit();
-                        return true;
-                    case R.id.nav_tracker:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new TrackerFragment()).commit();
-                        return true;
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_contacts) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new activity_main_fragment_contacts()).commit();
+                    return true;
+                } else if (itemId == R.id.nav_notes) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new activity_main_fragment_notes()).commit();
+                    return true;
+                } else if (itemId == R.id.nav_schedule) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new activity_main_fragment_schedule()).commit();
+                    return true;
+                } else if (itemId == R.id.nav_tracker) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new activity_main_fragment_tracker()).commit();
+                    return true;
                 }
                 return false;
             }
